d35adb38d6ef4d81df67da77f563136611aee81e
Nick <kkkygsos@naver.com>
2024-06-22 23:48:15 +0900

    test
    test

--- DjangoServer/Stock/views.py
+++ DjangoServer/Stock/views.py
@@ -4,3 +4,15 @@ from .models import Post
 def home(request):
     posts = Post.objects.all()
     return render(request, 'home.html', {'posts': posts})
+
+import logging
+from django.http import HttpResponse
+
+logger = logging.getLogger('django')
+
+def my_view(request):
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    return HttpResponse("Logging test")
