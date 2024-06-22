c6d7e799a0142c1d2ddcbb128fa6cc2ec2701675
Nick <kkkygsos@naver.com>
2024-06-23 01:15:11 +0900

    test
    test

--- DjangoServer/Stock/urls.py
+++ DjangoServer/Stock/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('my-view/', views.my_view, name='my_view'),
-    path('', views.home, name='home'),
-]
+# urlpatterns = [
+#     path('my-view/', views.my_view, name='my_view'),
+#     # path('', views.home, name='home'),
+# ]
