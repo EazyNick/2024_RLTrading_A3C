bcac5e97148332660021bd3e790f57700041d332
Nick <kkkygsos@naver.com>
2024-06-22 21:13:35 +0900

    django setting log추가
    240622 django setting log추가

--- DjangoServer/DjangoServer/settings.py
+++ DjangoServer/DjangoServer/settings.py
@@ -140,3 +140,22 @@ STATIC_ROOT = '/app/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/app/media'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django_debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
