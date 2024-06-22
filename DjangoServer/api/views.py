99744c4c85949911fc904476b6d3f63c22e6799a
Nick <kkkygsos@naver.com>
2024-06-23 01:32:45 +0900

    test
    test

--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -1,7 +1,9 @@
+# api/views.py
 from django.http import JsonResponse
 
-def home_view(request):
+def home(request):
     data = {
-        'key': 'value',  # 여기에 Flutter 애플리케이션으로 보낼 데이터를 넣습니다.
+        'message': 'Welcome to the home API!',
+        'status': 'success'
     }
     return JsonResponse(data)
