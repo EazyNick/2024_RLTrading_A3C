cf31a80a6ac5e9b5f68203dbdd22e90f4ddcc077
Nick <kkkygsos@naver.com>
2024-06-23 01:19:28 +0900

    test
    test

--- DjangoServer/DjangoServer/urls.py
+++ DjangoServer/DjangoServer/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls import handler404
-# from Stock import views as stock_views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('stock/', include('Stock.urls'))
 ]
-
-# handler404 = stock_views.custom_404_view
