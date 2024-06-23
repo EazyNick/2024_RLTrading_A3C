2b4ff915b22632a54858bb21e714859c8064a0d9
Nick <kkkygsos@naver.com>
2024-06-23 18:40:15 +0900

    uwsgi_params추가
    uwsgi_params추가

--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -1,9 +1,13 @@
 # api/views.py
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 def home(request):
-    data = {
-        'message': 'Welcome to the home API!',
-        'status': 'success'
-    }
-    return JsonResponse(data)
+    return HttpResponse("Hello, world!")
+
+# def home(request):
+#     data = {
+#         'message': 'Welcome to the home API!',
+#         'status': 'success'
+#     }
+#     return JsonResponse(data)
