d8cd198bcbf80072106a759bfa429e6c89e138a3
Nick <kkkygsos@naver.com>
2024-06-24 11:28:00 +0900

    test5
    test5

--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -1,7 +1,14 @@
+# api/views.py
+
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 def home(request):
     context = {
-        'name': 'Your Name'
+        'name': 'Your Name',
+        'template_dirs': settings.TEMPLATES[0]['DIRS'],
+        'template_app_dirs': settings.TEMPLATES[0]['APP_DIRS'],
+        'template_path': os.path.join(settings.BASE_DIR, 'api/templates/api/home.html')
     }
-    return render(request, 'home.html', context)
+    return render(request, 'api/home.html', context)
