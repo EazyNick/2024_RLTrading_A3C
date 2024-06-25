54a3aca0414f6a34ae40006a1e79163b130b1ed7
Nick <kkkygsos@naver.com>
2024-06-25 11:00:20 +0900

    test
    test

--- DjangoServer/DjangoServer/core/celery.py
+++ DjangoServer/DjangoServer/core/celery.py
@@ -11,8 +11,11 @@ app = Celery('DjangoServer')
 # Django 설정에서 설정 불러오기
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Django 프로젝트의 모든 task 모듈을 로드합니다.
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# # Django 프로젝트의 모든 task 모듈을 로드합니다.
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# Django 프로젝트의 모든 task 모듈을 수동으로 로드합니다.
+app.autodiscover_tasks(['stock_app'])
 
 @app.task(bind=True)
 def debug_task(self):
