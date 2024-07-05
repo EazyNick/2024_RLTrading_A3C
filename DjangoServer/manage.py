845657256c24dac930b9064a316109b987f5f097
Nick <kkkygsos@naver.com>
2024-07-05 22:22:12 +0900

    test
    test

--- DjangoServer/manage.py
+++ DjangoServer/manage.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import threading
 import time
 
+# test 주석
+
 try:
     sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
     # from mainrun import Run 
