4b727f08c84de774e0814dab684c8ceee5461c92
Nick <kkkygsos@naver.com>
2024-07-08 21:55:00 +0900

    rest api test
    rest api test

--- DjangoServer/DjangoServer/urls.py
+++ DjangoServer/DjangoServer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # 'api' 앱의 URL 포함
+    path('api/', include('api.urls')),         # 'api' 앱의 URL 포함
+    path('accounts/', include('accounts.urls')) # 'accounts' 앱의 URL 포함
 ]
