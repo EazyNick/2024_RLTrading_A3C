354aaa7eb1d204f66f9b38c97ddd0ed1ff553f4f
Nick <kkkygsos@naver.com>
2024-07-08 21:19:29 +0900

    Revert "api test"
    This reverts commit 4f2aaf7a738d1d17fc7f11b704d721b109bdfd55.

--- DjangoServer/api/urls.py
+++ DjangoServer/api/urls.py
@@ -1,7 +1,13 @@
-# api/urls.py
-from django.urls import path
-from .views import UserCreate
+from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MyModelViewSet
+
+router = DefaultRouter()
+router.register(r'mymodel', MyModelViewSet)
 
 urlpatterns = [
-    path('register/', UserCreate.as_view(), name='user-register'),
-]
+    path('', include(router.urls)),
+    path('home/', views.home, name='home'),
+]   
+
--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -1,8 +1,27 @@
 # api/views.py
-from rest_framework import generics
-from django.contrib.auth.models import User
-from .serializers import UserSerializer
 
-class UserCreate(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .models import MyModel
+from .serializers import MyModelSerializer
+from rest_framework.renderers import JSONRenderer
+
+class MyModelViewSet(viewsets.ModelViewSet):
+    queryset = MyModel.objects.all()
+    serializer_class = MyModelSerializer
+    renderer_classes = [JSONRenderer]
+
+import logging
+
+logger = logging.getLogger('mylogger')
+
+def home(request):
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
+    return render(request, 'api/home.html')
+
+    
