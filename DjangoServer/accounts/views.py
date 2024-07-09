37ec184739b5f71d1f268b5657a0d5a6d51920d1
Nick <kkkygsos@naver.com>
2024-07-09 22:09:40 +0900

    rest api test
    rest api test

--- DjangoServer/accounts/views.py
+++ DjangoServer/accounts/views.py
@@ -5,12 +5,8 @@ from .serializers import UserSerializer
 
 class RegisterView(APIView):
     def post(self, request):
-        print("RegisterView POST request received")
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            print("Serializer is valid")
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print("Serializer is invalid")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
