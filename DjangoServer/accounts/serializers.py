b381d2e5f9e99af77b5a04863937f7b07719a69e
Nick <kkkygsos@naver.com>
2024-07-09 21:50:29 +0900

    rest api test
    rest api test

--- DjangoServer/accounts/serializers.py
+++ DjangoServer/accounts/serializers.py
@@ -2,17 +2,15 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            password=validated_data['password']
         )
-        user.set_password(validated_data['password'])
-        user.save()
         return user
