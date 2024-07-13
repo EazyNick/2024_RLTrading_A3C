67ffd50bee874f0ea8c28e34f1cddaf3f2b37bf7
Nick <kkkygsos@naver.com>
2024-07-13 23:20:43 +0900

    rest api test
    rest api test

--- DjangoServer/api/urls.py
+++ DjangoServer/api/urls.py
@@ -4,5 +4,5 @@ from .views import SaveItemView, LoginView, AccountStatusView
 urlpatterns = [
     path('accounts/', SaveItemView.as_view(), name='account-save'),
     path('accounts/login/', LoginView.as_view(), name='account-login'),  # 로그인 경로 추가
-    path('api/account-status/', AccountStatusView.as_view(), name='account-status'),
+    path('account/status/', AccountStatusView.as_view(), name='account-status'),
 ]
