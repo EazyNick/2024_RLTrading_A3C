a0fb0bd76d259836cd6c1d655274f0c470ee11bf
Nick <kkkygsos@naver.com>
2024-07-13 20:01:56 +0900

    login rest api test
    login rest api test

--- DjangoServer/api/urls.py
+++ DjangoServer/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SaveItemView
+from .views import SaveItemView, LoginView
 
 urlpatterns = [
-    path('items/', SaveItemView.as_view(), name='item-save'),
+    path('accounts/', SaveItemView.as_view(), name='account-save'),
+    path('accounts/login/', LoginView.as_view(), name='account-login'),  # 로그인 경로 추가
 ]
--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -1,9 +1,11 @@
 import time
 import boto3
+from boto3.dynamodb.conditions import Key
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.renderers import JSONRenderer
+
 
 class SaveItemView(APIView):
     renderer_classes = [JSONRenderer]
@@ -33,5 +35,34 @@ class SaveItemView(APIView):
                 }
             )
             return Response({"message": "Data saved to DynamoDB successfully"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class LoginView(APIView):
+    renderer_classes = [JSONRenderer]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password:
+            return Response({"error": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # DynamoDB 클라이언트 초기화
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+        table = dynamodb.Table('Accounts')
+
+        try:
+            response = table.query(
+                IndexName='email-index',  # 이메일을 기준으로 인덱스를 사용하여 조회
+                KeyConditionExpression=Key('email').eq(email)
+            )
+            items = response.get('Items', [])
+            if not items:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+            user = items[0]
+            if user['password'] != password:
+                return Response({"error": "Incorrect password"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
