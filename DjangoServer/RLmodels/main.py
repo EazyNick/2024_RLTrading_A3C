4de5bbd0300ecb20604712bef13c7003a861052c
Nick <kkkygsos@naver.com>
2024-07-26 17:24:13 +0900

    Update main.py

--- DjangoServer/RLmodels/main.py
+++ DjangoServer/RLmodels/main.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 import sys
 import os
 from pathlib import Path
+from modules.utils import *
 
 # 프로젝트 루트 경로를 추가
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,7 +15,6 @@ sys.path.append(str(Path(__file__).resolve().parents[2] / 'modules'))
 try:
     from Agent.A3CAgent import A3CAgent  # A3CAgent 클래스 불러오기
     from env.env import StockTradingEnv
-    from modules.utils import *
 except Exception as e:
     print(f"import error {e}")
 
