2594cce2645f8b92092e0aacaf5e903a6755544b
Nick <kkkygsos@naver.com>
2024-07-28 20:38:23 +0900

    Update settings.py

--- DjangoServer/DjangoServer/settings.py
+++ DjangoServer/DjangoServer/settings.py
@@ -52,11 +52,11 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1',  '13.210.203.153', 'fintech19190301.k
 CELERY_BEAT_SCHEDULE = {
     'run-task-every-60-seconds': {
         'task': 'stock_app.tasks.run_task',
-        'schedule': 65.0,  # 60초마다 실행
+        'schedule': 10.0,  # 60초마다 실행
     },
     'run-task2-every-30-seconds': {
         'task': 'stock_app.tasks2.run_task2',
-        'schedule': 10.0,  # 30초마다 실행
+        'schedule': 600,  # 30초마다 실행
     },
 }
 
