410a620da19518e01397d5c1bc9bd920503a88b1
Nick <kkkygsos@naver.com>
2024-08-03 19:10:54 +0900

    Update views.py

--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -89,6 +89,7 @@ class LoginView(APIView):
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class AccountStatusView(View):
+    @csrf_exempt
     def get(self, request):
         account_id = request.GET.get('account_id')
 
