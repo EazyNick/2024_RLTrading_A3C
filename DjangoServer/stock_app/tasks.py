c918babf5adb2dd261e4364cba48650a644d0481
Nick <kkkygsos@naver.com>
2024-08-26 19:39:47 +0900

    Update tasks.py

--- DjangoServer/stock_app/tasks.py
+++ DjangoServer/stock_app/tasks.py
@@ -124,8 +124,8 @@ def run_task():
         log_manager.logger.info(f"filtered_buy_sell_log: {filtered_buy_sell_log}")
 
         # -1000 ~ 1000사이의 점수 반환, GPT 스코어 반환함수로, 비용문제로 주석처리 해둠
-        score = API_main()
-        # score = 50
+        # score = API_main()
+        score = 50
 
         # chat gpt의 매매 점수가 900이상(과매수)
         if score < 900:
