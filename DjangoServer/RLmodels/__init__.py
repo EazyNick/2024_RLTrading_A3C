e56e3bc43361e8d34e704ea142f6c5abff374491
Nick <kkkygsos@naver.com>
2024-09-06 14:52:08 +0900

    Update __init__.py

--- DjangoServer/RLmodels/__init__.py
+++ DjangoServer/RLmodels/__init__.py
@@ -1,4 +1,7 @@
 from .data.DynamoDB.dynamodb_to_csv import convert_dynamodb_to_csv
+from .Agent.A3CAgent import A3CAgent, update_global_model
 
 __all__ = ['convert_dynamodb_to_csv',
+           'A3CAgent',
+           'update_global_model',
            ]
